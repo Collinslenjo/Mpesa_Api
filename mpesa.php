92f141bc22a1e31967cd7f4be06acd70d2a4b3b6
collins lenjo <collinslenjo.cl@gmail.com>
2017-12-03 18:43:50 +0300

    more functions added

--- mpesa.php
+++ mpesa.php
@@ -4,4 +4,8 @@ require 'vendor/autoload.php';
 class Mpesa{
 	private function __construct(){}
 	public function authenticate(){}
+	public function c2b_request(){}
+	public function b2c_request(){}
+	public function notifications(){}
+	public function callback_url(){}
 }
